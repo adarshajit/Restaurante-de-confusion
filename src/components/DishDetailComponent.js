8ef8be157b8bd3f8caa96f9a8b9aa6052b79fdf6
adarsh_ajit <adarshajit@gmail.com>
2020-08-20 22:07:04 +0530

    updated the dishdetail component to show the dish info once clicked

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,72 +1,72 @@
 import React, { Component } from 'react';
-import { Card,CardBody,CardText,CardTitle,CardImg } from 'reactstrap';
+import { Card,CardBody,CardText,CardTitle,CardImg,Breadcrumb,BreadcrumbItem } from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 
 
     function RenderDish({dish}){
-        if(dish != null){
-            return(
-                    <Card>
-                        <CardImg 
-                            width = "100%" 
-                            src={dish.image} 
-                            alt={dish.name} 
-                        />
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                );
-    }else{
-        return(
-            <div></div>
-        )
-
-    }
+      return(
+          <div className ="col-12 col-md-5 m-1">
+              <Card>
+                  <CardImg top src={dish.image} alt={dish.name} />
+                  <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                  </CardBody>
+              </Card>
+          </div>
+      )
+   
 }
-    function RenderComments({dish}){
-        if(dish != null){
-            let list = dish.comments.map((comment) =>{
-                return(
-                    <div key={comment.id}>
-                    <Card>
-                        <CardBody>
-                            <CardTitle><h5>{comment.comment}</h5></CardTitle>
-                            <CardText>--{comment.author},{comment.date}</CardText>
-                        </CardBody>
-                    </Card>
-    
-                </div>
-                )
-            })
-        return(
-            <div>
-                <h2>Comments</h2>
-                {list}
-            </div> )
-        }
-        else{
+    function RenderComments({comments}){
+        if(comments != null){
             return(
-            <div></div>)
+                <div className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                        {comments.map((comment)=>{
+                            return(
+                                <li key={comment.id}>
+                                    <p>{comment.comment}</p>
+                                    <p>--{comment.author}, {comment.date}</p>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            )
         }
+
     }
 
     const DishDetail = (props)=>{
-        return(
+    if(props.dish!=null)
+        return (
             <div className="container">
                 <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish}/>
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderComments dish={props.dish}/>
-                    </div>
-                    
+                    <Breadcrumb>
 
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
                 </div>
-
-            </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments} />
+                    </div>
+                </div>
+                </div>
+        )
+    else
+        return(
+            <div></div>
         )
     }
 
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import { Card, CardImgOverlay,CardTitle,CardImg } from 'reactstrap';
+import React from 'react'
+import { Card, CardImg, CardImgOverlay,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-
-
-
-function RenderMenuItem({dish, onClick}){
-    return(
-        <Card key={dish.id} 
-        onClick={() => onClick(dish.id)}>
-            <CardImg width = "100%" src={dish.image} alt={dish.name} />
-            <CardImgOverlay>
-                <CardTitle>{dish.name}</CardTitle>
-            </CardImgOverlay>
-        </Card>
-    )
+    function RenderMenuItem ({dish, onClick}) {
+        return (
+            <Card>
+                <Link to={`/menu/${dish.id}`} >
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Link>
+            </Card>
+        );
     }
+    
 
     const Menu = (props) =>{
         const menu = props.dishes.map((dish)=>{
@@ -26,13 +27,23 @@ function RenderMenuItem({dish, onClick}){
             );
         })
     
-            return (
-                <div className="container">
-                  <div className="row">
-                    {menu}
-                  </div>
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>                
                 </div>
-              );    
+                <div className="row">
+                    {menu}
+                </div>
+            </div>
+        );
     }
   
 export default Menu
