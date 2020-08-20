7be2feb5987fb6eea4a456158f3863907eeb951c
adarsh_ajit <adarshajit@gmail.com>
2020-08-20 14:56:34 +0530

    updated the home component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,6 +6,9 @@ import Menu from './MenuComponent'
 import Contact from './ContactComponent';
 import DishDetail from './DishDetailComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect} from 'react-router-dom';
 
 
@@ -14,6 +17,9 @@ class Main extends Component{
     super(props);
     this.state = {
       dishes : DISHES,
+      comments: COMMENTS,
+      promotions : PROMOTIONS,
+      leaders : LEADERS
  
     }
   }
@@ -23,7 +29,11 @@ class Main extends Component{
   render(){
     const HomePage= ()=>{
       return(
-        <Home />
+        <Home 
+        dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+        promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+        leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+    />
       )
     }
     return (
