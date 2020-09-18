b75060c131e4712f27f4b408b6690e92ff03fad3
adarsh_ajit <adarshajit@gmail.com>
2020-09-18 20:48:12 +0530

    form validation to submit comments

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -20,7 +20,7 @@ import { Link } from 'react-router-dom';
     const Menu = (props) =>{
         const menu = props.dishes.map((dish)=>{
             return(
-                <div className="col-12 col-md-5 mt-1">
+                <div className="col- col-md-5 mt-1">
                 <RenderMenuItem dish={dish} onClick={props.onClick}/>
                    
                 </div>
@@ -34,7 +34,8 @@ import { Link } from 'react-router-dom';
                         <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
                         <BreadcrumbItem active>Menu</BreadcrumbItem>
                     </Breadcrumb>
-                    <div className="col-12">
+                    
+                    <div className="col-5">
                         <h3>Menu</h3>
                         <hr />
                     </div>                
