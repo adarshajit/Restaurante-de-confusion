fed17a4d836eed2ac35069c64fb37fd9bcb68a4e
adarsh_ajit <adarshajit@gmail.com>
2020-09-18 12:37:13 +0530

    react redux implemented

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-social/bootstrap-social.css';
 import {BrowserRouter} from 'react-router-dom';
 import Main from './components/MainComponent';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 
 class App extends Component{
@@ -12,11 +16,13 @@ class App extends Component{
 
   render(){
   return (
+    <Provider store={store}>
     <BrowserRouter>
-    <div>
-      <Main />
-    </div>
+      <div className="App">
+        <Main />
+      </div>
     </BrowserRouter>
+  </Provider>
   );
   }
 }
