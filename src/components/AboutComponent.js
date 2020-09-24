d8a1e31d6c7aef4f3a991794c0e91fa4b2bb6896
adarsh_ajit <adarshajit@gmail.com>
2020-09-24 23:22:18 +0530

    Assignment 4 completed

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,27 +1,67 @@
 import React from "react";
-import {Breadcrumb,BreadcrumbItem,Card,CardBody,CardHeader,Media} from "reactstrap";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  CardBody,
+  CardHeader,
+  Media
+} from "reactstrap";
 import { Link } from "react-router-dom";
+import { FadeTransform, Fade, Stagger } from "react-animation-components";
+
+import { Loading } from "./LoadingComponent";
+import { baseUrl } from "../shared/baseUrl";
+
+function RenderLeader(props) {
+  return (
+    <Media>
+      <Media left middle href="#">
+        <Media
+          object
+          src={baseUrl + props.leader.image}
+          alt={props.leader.name}
+        />
+      </Media>
+      <Media body className="pl-5">
+        <Media heading>{props.leader.name}</Media>
+        <p>{props.leader.designation}</p>
+        <p>{props.leader.description}</p>
+      </Media>
+    </Media>
+  );
+}
 
 function About(props) {
-  const RenderLeader = () => {
-    const leaders = props.leaders.map((leader) => {
-      return (
-        <div key={leader.id} className="col-12 mt-5">
-          <Media tag="li">
-            <Media left>
-              <Media object src={leader.image} alt={leader.name} />
-            </Media>
-            <Media body className="ml-5">
-              <Media heading>{leader.name}</Media>
-              <p>{leader.designation}</p>
-              {leader.description}
-            </Media>
-          </Media>
+  let leaders = "";
+  if (props.leaders.isLoading) {
+    leaders = (
+      <div className="container">
+        <div className="row">
+          <Loading />
         </div>
+      </div>
+    );
+  } else if (props.leaders.errMess) {
+    leaders = (
+      <div className="container">
+        <div className="row">
+          <h4>{props.errMess}</h4>
+        </div>
+      </div>
+    );
+  } else if (props.leaders.leaders) {
+    leaders = props.leaders.leaders.map((leader, i) => {
+      return (
+        <Fade in>
+          <li className="list-unstyled">
+            <RenderLeader key={i} leader={leader}></RenderLeader>
+          </li>
+        </Fade>
       );
     });
-    return leaders;
-  };
+  }
+
   return (
     <div className="container">
       <div className="row">
@@ -95,11 +135,11 @@ function About(props) {
       </div>
       <div className="row row-content">
         <div className="col-12">
-          <h2>Corporate Leadership</h2>
+          <h2 className="text-left">Corporate Leadership</h2>
         </div>
         <div className="col-12">
           <Media list>
-            <RenderLeader />
+            <Stagger in>{leaders}</Stagger>
           </Media>
         </div>
       </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,19 +1,26 @@
-import React, { Component } from 'react';
-import Header from './HeaderComponent';
-import Home from './HomeComponent';
-import About from './AboutComponent';
-import Footer from './FooterComponent';
-import Menu from './MenuComponent'
-import Contact from './ContactComponent';
-import DishDetail from './DishDetailComponent';
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
-import { connect } from 'react-redux';
-import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
-import { actions } from 'react-redux-form';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import React, { Component } from "react";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import { actions } from "react-redux-form";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
+import Home from "./HomeComponent";
+import Contact from "./ContactComponent";
+import About from "./AboutComponent";
+import Menu from "./MenuComponent";
+import DishDetail from "./DishDetailComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
 
-
+import { connect } from "react-redux";
+import {
+  postComment,
+  addComment,
+  fetchDishes,
+  fetchComments,
+  fetchPromos,
+  fetchLeaders,
+  postFeedback
+} from "../redux/ActionCreators";
 
 const mapStateToProps = state => {
   return {
@@ -21,76 +28,133 @@ const mapStateToProps = state => {
     comments: state.comments,
     promotions: state.promotions,
     leaders: state.leaders
-  }
-}
+  };
+};
 
 const mapDispatchToProps = dispatch => ({
-  fetchDishes: () => { dispatch(fetchDishes())},
-  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
+  addComment: (dishId, rating, author, comment) =>
+    dispatch(addComment(dishId, rating, author, comment)),
+  postComment: (dishId, rating, author, comment) =>
+    dispatch(postComment(dishId, rating, author, comment)),
+  postFeedback: (
+    firstname,
+    lastname,
+    telnum,
+    email,
+    agree,
+    contactType,
+    message
+  ) =>
+    dispatch(
+      postFeedback(
+        firstname,
+        lastname,
+        telnum,
+        email,
+        agree,
+        contactType,
+        message
+      )
+    ),
+  fetchDishes: () => {
+    dispatch(fetchDishes());
+  },
+  resetFeedbackForm: () => {
+    dispatch(actions.reset("feedback"));
+  },
   fetchComments: () => dispatch(fetchComments()),
   fetchPromos: () => dispatch(fetchPromos()),
-  postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment))
+  fetchLeaders: () => dispatch(fetchLeaders())
 });
 
-
-
-class Main extends Component{
-
-
+class Main extends Component {
+  onDishSelect(dishId) {
+    this.setState({ selectedDishId: dishId });
+  }
 
   componentDidMount() {
     this.props.fetchDishes();
     this.props.fetchComments();
     this.props.fetchPromos();
+    this.props.fetchLeaders();
   }
-  
-  
- 
-  render(){
-    const HomePage = () => {
-      return(
-        <Home 
-        dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
-        dishesLoading={this.props.dishes.isLoading}
-        dishErrMess={this.props.dishes.errMess}
-        promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
-        promoLoading={this.props.promotions.isLoading}
-        promoErrMess={this.props.promotions.errMess}
-        leader={this.props.leaders.filter((leader) => leader.featured)[0]}
-    />
-      );
-    }
 
-    const DishWithId = ({match}) => {
-      return(
-        <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-        isLoading={this.props.dishes.isLoading}
-        errMess={this.props.dishes.errMess}
-        comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
-        commentsErrMess={this.props.comments.errMess}
-        addComment={this.props.addComment}
-        postComment={this.props.postComment}
-      />
+  render() {
+    const HomePage = () => {
+      return (
+        <Home
+          dish={this.props.dishes.dishes.filter(dish => dish.featured)[0]}
+          dishesLoading={this.props.dishes.isLoading}
+          dishesErrMess={this.props.dishes.errMess}
+          promotion={
+            this.props.promotions.promotions.filter(promo => promo.featured)[0]
+          }
+          promoLoading={this.props.promotions.isLoading}
+          promoErrMess={this.props.promotions.errMess}
+          leader={
+            this.props.leaders.leaders.filter(leader => leader.featured)[0]
+          }
+        />
       );
     };
 
-
-
+    const DishWithId = ({ match }) => {
+      return (
+        <DishDetail
+          dish={
+            this.props.dishes.dishes.filter(
+              dish => dish.id === parseInt(match.params.dishId, 10)
+            )[0]
+          }
+          isLoading={this.props.dishes.isLoading}
+          errMess={this.props.dishes.errMess}
+          comments={this.props.comments.comments.filter(
+            comment => comment.dishId === parseInt(match.params.dishId, 10)
+          )}
+          commentsErrMess={this.props.comments.errMess}
+          postComment={this.props.postComment}
+        />
+      );
+    };
     return (
       <div>
         <Header />
-        <TransitionGroup>
-            <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+        <div>
+          <TransitionGroup>
+            <CSSTransition
+              key={this.props.location.key}
+              classNames="page"
+              timeout={300}
+            >
               <Switch location={this.props.location}>
-                  <Route path='/home' component={HomePage} />
-                  <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />} />
-                  <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
-                  <Route path='/menu/:dishId' component={DishWithId} />
-                  <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
-                  <Redirect to="/home" />
+                <Route path="/home" component={HomePage} />
+                <Route
+                  exact
+                  path="/aboutus"
+                  component={() => <About leaders={this.props.leaders} />}
+                />
+                } />
+                <Route
+                  exact
+                  path="/menu"
+                  component={() => <Menu dishes={this.props.dishes} />}
+                />
+                <Route path="/menu/:dishId" component={DishWithId} />
+                <Route
+                  exact
+                  path="/contactus"
+                  component={() => (
+                    <Contact
+                      resetFeedbackForm={this.props.resetFeedbackForm}
+                      postFeedback={this.props.postFeedback}
+                    />
+                  )}
+                />
+                <Redirect to="/home" />
               </Switch>
             </CSSTransition>
           </TransitionGroup>
+        </div>
         <Footer />
       </div>
     );
@@ -98,6 +162,3 @@ class Main extends Component{
 }
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
-
-
-
